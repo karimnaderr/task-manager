9ec1e8066b11c7bb2b77b05d5f2433d02de52c41
karimnaderr <naderkarim98@gmail.com>
2025-11-06 01:32:21 +0200

    rate limiting

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { login, register, getMe } from "../controllers/authController";
 import { authenticateToken } from "../middleware/authMiddleware";
+import { authRateLimiter } from "../middleware/rateLimiter";
 
 const router = Router();
 
-router.post("/login", login);
-router.post("/register", register);
-router.get("/me", authenticateToken, getMe);
 
+router.post("/login", authRateLimiter, login);
+router.post("/register", authRateLimiter, register)
+router.get("/me", authenticateToken, getMe);
 
 export default router;
